ac466b60bfa00ea2ad5c2e3c86c8bc6a83649206
thisislawatts <luke@thisis.la>
2016-01-13 16:52:33 +0000

    Adds a Pause

--- showMe.js
+++ showMe.js
@@ -17,11 +17,23 @@ var ShowMe = function() {
   _self.next = document.getElementById('SNext');
   _self.prev = document.getElementById('SPrev');
   _self.pause = document.getElementById('SPause');
+  _self.Controls = document.getElementById('Controls');
+  _self.openControls = document.querySelector('.sm-open');
+  _self.closeControls = document.querySelector('.sm-close');
 
   _self.frameSize = {
     width: _self.Frame.offsetWidth,
     height: _self.Frame.offsetHeight
   };
+
+  _self.closeControls.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    _self.Controls.classList.toggle('s__active');
+  });
+  _self.openControls.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    _self.Controls.classList.toggle('s__active');
+  });
 
   _self.pause.addEventListener('click', function() {
     var inner = _self.pause.innerText.toLowerCase();
@@ -39,19 +51,24 @@ var ShowMe = function() {
     _self.updateUrl();
   }, false );
 
-  _self.updateUrl();
+  _self.setupUrl();
+
   // Calculate Scale
   _self.resize();
   _self.sizes = _self.getSizes();
   _self.sizePointer = 0;
 
+  _self.Select = _self.getSizesAsSelect();
+  _self.Details.appendChild(_self.Select);
+
+  _self.Select.addEventListener('change', function(evt) {
+    var si = evt.target.value.split(',');
+    _self.setSize(si[0], si[1]);
+  }, false);
+
   _self.stepSize();
   
   return this;
-};
-
-ShowMe.prototype.getPercentage = function(scalar) {
-  return scalar === 1 ? 100 : (scalar * 100).toPrecision(2);
 };
 
 ShowMe.prototype.stepSize = function() {
@@ -59,9 +76,13 @@ ShowMe.prototype.stepSize = function() {
 
   var s = _self.sizes[_self.sizePointer];
 
+  console.log('Step size:', s);
   if (s) {
-    _self.setSize( s[0], s[1] );
-    this.Details.innerHTML = (_self.sizePointer + 1) + '. ' + s[0] + '&times' + s[1] + 'px / ' + _self.getPercentage( this.scalar ) + '%';
+    _self.setSize( s.dimensions[0], s.dimensions[1] );
+    console.log('New Size:', document.getElementById(s.id) );
+
+    // Select latest item
+    document.getElementById(s.id).setAttribute('selected',true);
   }
 
   if (_self.sizePointer < _self.sizes.length ) {
@@ -111,19 +132,33 @@ ShowMe.prototype.getUrl = function() {
   return this.Url.value;
 };
 
+ShowMe.prototype.setupUrl = function() {
+  var localUrl = window.localStorage.getItem('BCMHShowMeUrl');
+  this.iFrame.src = localUrl || this.getUrl();
+
+  if (localUrl)
+    this.Url.value = localUrl;
+};
+
 ShowMe.prototype.updateUrl = function() {
   this.iFrame.src = this.getUrl();
+  window.localStorage.setItem('BCMHShowMeUrl', this.getUrl() );
 };
 
 ShowMe.prototype.getSizes = function() {
   var sizes = document.getElementById('Sizes').innerHTML;
-  var parts = sizes.split('|');
+  var parts = sizes.split('|'),
+      obj = [];
 
   for (var i = 0; i < parts.length; i++) {
-    parts[i] = parts[i].split('x');
+    var s = parts[i].split('x');
+    obj.push({
+      'id'          : this.getID(s),
+      'dimensions'  : s
+    });
   }
 
-  return parts;
+  return obj;
 };
 
 ShowMe.prototype.setSize = function(w, h) {
@@ -133,6 +168,42 @@ ShowMe.prototype.setSize = function(w, h) {
   this.resize(w, h);
 };
 
+ShowMe.prototype.getSizesAsSelect = function() {
+  var select = document.createElement('select'),
+    inner = '',
+    sizes = this.getSizes(),
+    counter = 1;
+
+  console.log('Sizes:', sizes );
+
+  for (var i = 0; i < sizes.length; i++) {
+    var s = sizes[i];
+    console.log(s);
+    
+    inner += '<option id="' + s.id  + '" value="' + s.dimensions[0] + ',' + s.dimensions[1] + '">' + (i + 1) + '. ' + s.dimensions[0] + '&times' + s.dimensions[1] + 'px / ' + this.getPercentage(this.getScalar( {width: s.dimensions[0], height: s.dimensions[1]} )) + '%' + '</option>';
+  }
+
+  select.innerHTML = inner;
+
+  return select;
+};
+
+/**
+ *
+ * 
+ * @param  Array size
+ * @return String
+ */
+ShowMe.prototype.getID = function(size) {
+  return 's' + size[0] + 'x' + size[1];
+};
+
+/**
+ * 
+ * 
+ * @param  Array targetSize [description]
+ * @return {[type]}            [description]
+ */
 ShowMe.prototype.getScalar = function( targetSize ) {
   var scaleX = (window.innerWidth - this.padding * 2 ) / targetSize.width,
       scaleY = (window.innerHeight - this.padding * 2 ) / targetSize.height,
@@ -145,6 +216,10 @@ ShowMe.prototype.getScalar = function( targetSize ) {
   this.scalar = scalar;
 
   return scalar;
+};
+
+ShowMe.prototype.getPercentage = function(scalar) {
+  return scalar === 1 ? 100 : (scalar * 100).toPrecision(2);
 };
 
 var s = new ShowMe();
